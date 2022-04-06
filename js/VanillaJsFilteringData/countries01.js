a0c66ce74b57c336e2a3e1f792f78d1303c3b62d
RovsenRzali <rovsen_204@mail.ru>
2022-04-06 22:31:02 +0400

    Add task CountriesName01

---
+++ js/VanillaJsFilteringData/countries01.js
@@ -0,0 +1,108 @@
+countries = {
+  Afghanistan: [
+      "Herat",
+      "Kabul",
+      "Kandahar",
+      "Molah",
+      "Rana",
+      "Shar",
+      "Sharif",
+      "Wazir Akbar Khan"
+  ],
+  Albania: [
+      "Elbasan",
+      "Petran",
+      "Pogradec",
+      "Shkoder",
+      "Tirana",
+      "Ura Vajgurore"
+  ],
+  Algeria: [
+      "Algiers",
+      "Annaba",
+      "Azazga",
+      "Batna City",
+      "Blida",
+      "Bordj",
+      "Bordj Bou Arreridj",
+      "Bougara",
+      "Cheraga",
+      "Chlef",
+      "Constantine",
+      "Djelfa",
+      "Draria",
+      "El Tarf",
+      "Hussein Dey",
+      "Illizi",
+      "Jijel",
+      "Kouba",
+      "Laghouat",
+      "Oran",
+      "Ouargla",
+      "Oued Smar",
+      "Relizane",
+      "Rouiba",
+      "Saida",
+      "Souk Ahras",
+      "Tamanghasset",
+      "Tiaret",
+      "Tissemsilt",
+      "Tizi",
+      "Tizi Ouzou",
+      "Tlemcen"
+  ],
+  Andorra: [
+      "Andorra la Vella",
+      "Canillo",
+      "Encamp",
+      "Engordany",
+      "Escaldes-Engordany",
+      "La Massana",
+      "Llorts",
+      "Ordino",
+      "Santa Coloma",
+      "Sispony",
+      "Soldeu"
+  ],
+  Angola: [
+      "Ambriz",
+      "Benguela",
+      "Cabinda",
+      "Cacole",
+      "Camabatela",
+      "Cazeta",
+      "Huambo",
+      "Kuito",
+      "Lobito",
+      "Luanda",
+      "Lubango",
+      "Lucapa",
+      "Lumeje",
+      "Malanje",
+      "Menongue",
+      "Muxaluando",
+      "Namibe",
+      "Ondjiva",
+      "Piri",
+      "Saurimo",
+      "Talatona"
+  ],
+  Antigua: [
+      "All Saints",
+      "Cassada Gardens",
+      "Codrington",
+      "Old Road",
+      "Parham",
+      "Woods"
+  ],
+}
+
+
+
+ function showCountriesName () { for (let code in countries) {
+  console.log(code)
+}
+ }
+
+ showCountriesName ()
+
