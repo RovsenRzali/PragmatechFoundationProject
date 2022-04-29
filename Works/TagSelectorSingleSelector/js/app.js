9ffb20c732c410ece03a28d889903d26a16948f5
RovsenRzali <rovsen_204@mail.ru>
2022-04-29 23:55:22 +0400

    Add TagSelectorSingleSelector

---
+++ Works/TagSelectorSingleSelector/js/app.js
@@ -0,0 +1,42 @@
+function goalert(){
+  //alert('Bu Javascripti oyren')
+
+}
+
+
+filmNamesArray = [
+  "Lost of Bride",
+  "Last Blood",
+  "Fast and Farious",
+  "Taxi",
+  "White Collar"
+]
+
+
+
+function showValid(){
+  for(i = 0; i<filmNamesArray.length + 1; i++) {
+  const valid = document.getElementsById('film-names')[i]
+   valid.classList.toggle("material-symbols-outlined");
+  let text = '<i class="fa fa-circle-check"></i>';
+  valid.innerHTML += text
+  }
+
+}
+
+function myFunction() {
+  var element = document.getElementById("myDIV");
+  element.classList.toggle("mystyle");
+}
+
+const filmNamesContainer = document.querySelector('.film-names-container');
+const filmNames = document.querySelector('.film-names');
+
+for (let names of filmNamesArray){
+  let filmNames = `
+  <div class="film-names">
+  <h4>${names}</h4>
+  </div>`
+
+filmNamesContainer.innerHTML += filmNames;
+}
