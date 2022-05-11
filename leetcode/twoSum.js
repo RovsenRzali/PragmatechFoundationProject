344a7d63be423c91c10a8006a8827668f026e100
Rovshan Rzali <96679912+RovsenRzali@users.noreply.github.com>
2022-05-12 00:28:22 +0400

    Update twoSum.js

--- leetcode/twoSum.js
+++ leetcode/twoSum.js
@@ -6,7 +6,7 @@ function compareTriplets(nums) {
   let outPut = []
 
   for (let i = 0; i<=nums.length; i++){
-    for (let j = 1; j<=nums.length; j++){
+    for (let j = i+1; j<=nums.length; j++){
 
     if(nums[i] + nums[j] == target) {
       outPut.push(i,j)
