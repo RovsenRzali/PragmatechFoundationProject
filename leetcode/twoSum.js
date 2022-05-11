977b95b9982751318fcb50ce93624952f81e5454
RovsenRzali <rovsen_204@mail.ru>
2022-05-12 00:30:30 +0400

    Fix twoSum.js

--- leetcode/twoSum.js
+++ leetcode/twoSum.js
@@ -1,12 +1,12 @@
 
-nums = [2, 7, 13, 11];
+nums = [13, 11,2, 7];
 let target = 9;
 
 function compareTriplets(nums) {
   let outPut = []
 
-  for (let i = 0;i<=nums.length;i++){
-    for (let j = 1;j<=nums.length;j++){
+  for (let i = 0; i<=nums.length; i++){
+    for (let j = 1; j<=nums.length; j++){
 
     if(nums[i] + nums[j] == target) {
       outPut.push(i,j)
