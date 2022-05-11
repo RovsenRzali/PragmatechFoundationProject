daccdda072b1ea0a4ace8bcf7686f48aec74a06e
RovsenRzali <rovsen_204@mail.ru>
2022-05-12 00:04:01 +0400

    Add leetcode folder and solved Two Sum

--- Works/TagSelectorSingleSelector/js/app.js
+++ Works/TagSelectorSingleSelector/js/app.js
@@ -4,39 +4,39 @@ function goalert(){
 }
 
 
-filmNamesArray = [
-  "Lost of Bride",
-  "Last Blood",
-  "Fast and Farious",
-  "Taxi",
-  "White Collar"
-]
+// filmNamesArray = [
+//   "Lost of Bride",
+//   "Last Blood",
+//   "Fast and Farious",
+//   "Taxi",
+//   "White Collar"
+// ]
 
 
 
-function showValid(){
-  for(i = 0; i<filmNamesArray.length + 1; i++) {
-  const valid = document.getElementsById('film-names')[i]
-   valid.classList.toggle("material-symbols-outlined");
-  let text = '<i class="fa fa-circle-check"></i>';
-  valid.innerHTML += text
-  }
+// function showValid(){
+//   for(i = 0; i<filmNamesArray.length + 1; i++) {
+//   const valid = document.getElementsById('film-names')[i]
+//    valid.classList.toggle("material-symbols-outlined");
+//   let text = '<i class="fa fa-circle-check"></i>';
+//   valid.innerHTML += text
+//   }
 
-}
+// }
 
-function myFunction() {
-  var element = document.getElementById("myDIV");
-  element.classList.toggle("mystyle");
-}
+// function myFunction() {
+//   var element = document.getElementById("myDIV");
+//   element.classList.toggle("mystyle");
+// }
 
-const filmNamesContainer = document.querySelector('.film-names-container');
-const filmNames = document.querySelector('.film-names');
+// const filmNamesContainer = document.querySelector('.film-names-container');
+// const filmNames = document.querySelector('.film-names');
 
-for (let names of filmNamesArray){
-  let filmNames = `
-  <div class="film-names">
-  <h4>${names}</h4>
-  </div>`
+// for (let names of filmNamesArray){
+//   let filmNames = `
+//   <div class="film-names">
+//   <h4>${names}</h4>
+//   </div>`
 
-filmNamesContainer.innerHTML += filmNames;
-}
+// filmNamesContainer.innerHTML += filmNames;
+// }
---
+++ leetcode/twoSum.js
@@ -0,0 +1,19 @@
+
+nums = [2, 7, 13, 11];
+let target = 9;
+
+function compareTriplets(nums) {
+  let outPut = []
+
+  for (let i = 0;i<=nums.length;i++){
+    for (let j = 1;j<=nums.length;j++){
+
+    if(nums[i] + nums[j] == target) {
+      outPut.push(i,j)
+    }
+  }
+  }
+     return outPut
+  }
+
+  console.log (compareTriplets(nums))
