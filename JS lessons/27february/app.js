960168279237e0c5a823d58957fe1fa8eb006819
RovsenRzali <rovsen_204@mail.ru>
2022-12-09 17:30:20 +0400

    Complete 27February lesson

--- JS lessons/27february/app.js
+++ JS lessons/27february/app.js
@@ -1,1 +1,13 @@
-let a = 5;
+// impitların təyin olunması.
+   // declare variables
+   // let, const, var
+
+let a =4 - "salam"
+console.log(a)
+
+// programin alqoritminin yazılması.
+
+
+
+
+// nəticənin idarə edilməsi.
