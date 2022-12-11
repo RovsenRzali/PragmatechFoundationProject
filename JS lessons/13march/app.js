95f8e9e925f0c5daae3132a89c1f11f9faa040cb
RovsenRzali <rovsen_204@mail.ru>
2022-12-11 12:52:01 +0400

    Coded till Logical OPerators

---
+++ JS lessons/13march/app.js
@@ -0,0 +1,29 @@
+// Number()
+// Boolean()
+// String()
+
+console.log('5'-true) // answ: =1
+
+let x = 5;
+let y = 6;
+let z = x+y
+let ad  = "Rovshan"
+console.log(x + '+' + y + '=' + z);
+console.log(`${x}+${y}=${z}`)
+console.log(`Dear Rovshan, It is result of  your operation. ${x}+${y}=${z}`)
+
+// == 
+ console.log('5' == 5) // true
+
+ // === 
+ console.log('5'=== 5) // false
+
+// Müqayisə operatorlari müqayisəni, hərfələrin ASCİİ cədvəlindəki qarşılığına əsasən aparır.
+ console.log('Abbas' > 'Samir')
+
+// Logical operators
+ console.log(x>4 && y<3) // and
+ console.log(x>4 || y<3) // or
+ console.log(!(x==5))
+
+ // leaved on 01.13
