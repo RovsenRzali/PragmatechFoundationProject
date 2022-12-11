3969f2b4f6c9f7ddec235e28da6988d091fbb0a8
RovsenRzali <rovsen_204@mail.ru>
2022-12-11 17:58:06 +0400

    Complete 13march lesson

---
+++ JS lessons/13march/calculator.js
@@ -0,0 +1,20 @@
+let x = false;
+let y = true;
+let operator = "+";
+
+if(x*1==x && y*1==y) {
+    if(operator == "+") {
+        console.log(`${x} + ${y} = ${x+y}`)
+    } else if(operator == "-") {
+        console.log(`${x} - ${y} = ${x-y}`)
+    } else if(operator == "*") {
+        console.log(`${x} * ${y} = ${x*y}`)
+    } else if(operator == "/") {
+        console.log(`${x} / ${y} = ${x/y}`)
+    } else {
+        console.log("Daxil etdiyiniz əməliyyyat riyazi operator deil.")
+    }
+} else {
+    console.log("Daxil etdiyiniz dəyərlərdən biri və ya hər ikizi ədəd deil.")
+}
+
