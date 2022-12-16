cbce754d42708ed6394fd40bf584a7f4634f0329
RovsenRzali <rovsen_204@mail.ru>
2022-12-16 12:10:17 +0400

    variable in global and local scope

--- JS lessons/19march/app.js
+++ JS lessons/19march/app.js
@@ -1,6 +1,45 @@
 let a = 45;
-let b = 34;
-let c = 12;
-let d = 56;
 
-// 00:29 cu dəqiqə.
+
+//console.log(Boolean(a=23))
+// scope da let-in yazılması ilə yazılmaması rasında böyük bir fərq var.
+
+if(a=4) {
+    let  a = 67;
+    console.log(a)
+} else{
+    let a = 12;
+    console.log(a)
+}
+
+console.log(a)
+
+if(a=4) {
+    a = 67;
+    console.log(a)
+} else{
+    let a = 12;
+    console.log(a)
+}
+
+console.log(a)
+
+// funsiyalarda da a-nın qabağına let yazmaqla yazmamaq arasında fərq var.
+function Foo() {
+     a = 45;
+    console.log(a)
+}
+
+Foo()   
+console.log(a)
+
+function Foo2() {
+    a = 56;
+}
+
+console.log(a)
+Foo2()   
+console.log(a)
+
+// 01:39 da qaldım.
+
