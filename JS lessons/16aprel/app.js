48986b3a1ed2838e42fc28ad9f1310b476b83948
RovsenRzali <rovsen_204@mail.ru>
2022-12-26 20:08:31 +0400

    16aprel lesson

---
+++ JS lessons/16aprel/app.js
@@ -0,0 +1,13 @@
+// Html elemenleri (CRUD)
+// C --creat
+// R --read
+// U --update
+// D --delete
+
+class Human {
+    constructor() {
+        this.name = 'Object'
+    }
+}
+
+const obj = new Human()
