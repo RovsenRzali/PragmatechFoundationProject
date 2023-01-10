d79e14254a805af4ad45639df329ab3e6f48dc7b
RovsenRzali <rovsen_204@mail.ru>
2023-01-10 16:39:45 +0400

    lesson124

---
+++ JS lessons/24aprel/app.js
@@ -0,0 +1,26 @@
+const slides = document.querySelector(".slides");
+
+let moveX = 0;
+
+function goRight() {
+  moveX -= 1200;
+  slides.style.transform = `translateX(${moveX}px)`;
+  if (moveX <= -4 * 1200) {
+    moveX = 0;
+  } else {
+    moveX -= 1200;
+    slides.style.transform = `translateX(${moveX}px)`;
+  }
+}
+
+function goLeft() {
+  //   moveX += 1200;
+  //   slides.style.transform = `translateX(${moveX}px)`;
+  if (moveX >= 0) {
+    moveX = -4 * 1200;
+    slides.style.transform = `translateX(${moveX}px)`;
+  } else {
+    moveX += 1200;
+    slides.style.transform = `translateX(${moveX}px)`;
+  }
+}
